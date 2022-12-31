fa3e3da89633a63686fcca92df22d80c0f45e398
Timdev <timur.sagimbaev@mail.ru>
2022-12-31 18:20:24 +0500

    update menu and add dice game

--- menu.py
+++ menu.py
@@ -1,8 +1,6 @@
 import pygame
-import sys
-import os
-import dice
 import menu2
+from useful import Button, load_image, terminate
 
 pygame.init()
 FPS = 50
@@ -12,40 +10,11 @@ buttons = pygame.sprite.Group()
 clock = pygame.time.Clock()
 
 
-def load_image(name):
-    fullname = os.path.join('data', name)
-    # если файл не существует, то выходим
-    if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
-        sys.exit()
-    image = pygame.image.load(fullname)
-    return image
-
-
-class Button(pygame.sprite.Sprite):
-    def __init__(self, pos, img, size):
-        super().__init__(buttons)
-        self.image = pygame.transform.scale(load_image(img), size)
-        self.rect = self.image.get_rect()
-        self.rect.x = pos[0]
-        self.rect.y = pos[1]
-
-    def click(self, pos):
-        if self.rect.collidepoint(pos[0], pos[1]):
-            return True
-        return  False
-
-
-def terminate():
-    pygame.quit()
-    sys.exit()
-
-
 def start_screen():
     fon = pygame.transform.scale(load_image("bg.png"), (WIDTH, HEIGHT))
     screen.blit(fon, (0, 0))
-    button_play = Button((700, 300), "play.png", (300, 200))
-    button_info = Button((950, 500), "info.png", (100, 100))
+    button_play = Button((700, 300), "play.png", (300, 200), buttons)
+    button_info = Button((950, 500), "info.png", (100, 100), buttons)
     
     while True:
         for event in pygame.event.get():
--- menu2.py
+++ menu2.py
@@ -1,41 +1,11 @@
 import pygame
-import sys
-import os
 import dice
+from useful import Button, load_image, terminate
 
 
 buttons = pygame.sprite.Group()
 clock = pygame.time.Clock()
 FPS = 50
-
-
-def terminate():
-    pygame.quit()
-    sys.exit()
-
-
-def load_image(name):
-    fullname = os.path.join('data', name)
-    # если файл не существует, то выходим
-    if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
-        sys.exit()
-    image = pygame.image.load(fullname)
-    return image
-
-
-class Button(pygame.sprite.Sprite):
-    def __init__(self, pos, img, size):
-        super().__init__(buttons)
-        self.image = pygame.transform.scale(load_image(img), size)
-        self.rect = self.image.get_rect()
-        self.rect.x = pos[0]
-        self.rect.y = pos[1]
-    
-    def click(self, pos):
-        if self.rect.collidepoint(pos[0], pos[1]):
-            return True
-        return  False
 
 
 def menu2(screen):
@@ -47,17 +17,16 @@ def menu2(screen):
     intro_rect.x = 400
     intro_rect.top = 20
     screen.blit(string_render, intro_rect)
-    dice_icon = Button((0, 315), "dice.png", (560, 315))
-    chess_icon = Button((560, 50), "chess.png", (560, 315))
-    voll_icon = Button((0, 30), "voleyball.png", (560, 315))
+    dice_icon = Button((0, 315), "dice.png", (560, 315), buttons)
+    chess_icon = Button((560, 50), "chess.png", (560, 315), buttons)
+    voll_icon = Button((0, 30), "voleyball.png", (560, 315), buttons)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if dice_icon.click(event.pos):
-                    pass
-                    #dice.start_game(screen)
+                    dice.start_game(screen)
                 if chess_icon.click(event.pos):
                     pass  # сюда впиши функцию активации игры
                 if voll_icon.click(event.pos):
